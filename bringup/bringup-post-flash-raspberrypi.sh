f2b7ebdcf897df0e80059a07cef603a2f38240c6
Taylor <tlalexander@gmail.com>
2022-01-18 02:32:37 -0800

    update post bringup flash script

--- bringup/bringup-post-flash-raspberrypi.sh
+++ bringup/bringup-post-flash-raspberrypi.sh
@@ -4,3 +4,5 @@ sudo sh get-docker.sh
 sudo usermod -aG docker pi
 sudo apt install vim libffi-dev libssl-dev python3-dev python3-pip
 sudo pip3 install docker-compose
+newgrp docker
+echo "Now run docker-compose -f docker-compose-vehicle.yml build in the acorn directory."
