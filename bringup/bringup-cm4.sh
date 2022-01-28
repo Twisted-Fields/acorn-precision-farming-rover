c5d5be3013345f7e39fa9b783fd56fa60879a458
Taylor <tlalexander@gmail.com>
2022-01-28 14:17:39 -0800

    Update bringup scripts.

--- bringup/bringup-cm4.sh
+++ bringup/bringup-cm4.sh
@@ -28,15 +28,16 @@ sudo cp overlay/config.txt /media/$USER/boot/
 sudo cp overlay/triple-sc16is752-spi1.dtbo /media/$USER/boot/overlays/
 sudo cp raspberry/99-gps.rules /media/$USER/rootfs/etc/udev/rules.d/
 
-rm -rf /media/$USER/rootfs/home/Bookshelf
-rm -rf /media/$USER/rootfs/home/Desktop
-rm -rf /media/$USER/rootfs/home/Documents
-rm -rf /media/$USER/rootfs/home/Downloads
-rm -rf /media/$USER/rootfs/home/Music
-rm -rf /media/$USER/rootfs/home/Pictures
-rm -rf /media/$USER/rootfs/home/Public
-rm -rf /media/$USER/rootfs/home/Templates
-rm -rf /media/$USER/rootfs/home/Videos
+rm -rf /media/$USER/rootfs/home/pi/Bookshelf
+rm -rf /media/$USER/rootfs/home/pi/Desktop
+rm -rf /media/$USER/rootfs/home/pi/Documents
+rm -rf /media/$USER/rootfs/home/pi/Downloads
+rm -rf /media/$USER/rootfs/home/pi/Music
+rm -rf /media/$USER/rootfs/home/pi/Pictures
+rm -rf /media/$USER/rootfs/home/pi/Public
+rm -rf /media/$USER/rootfs/home/pi/Templates
+rm -rf /media/$USER/rootfs/home/pi/Videos
+
 mkdir /media/$USER/rootfs/home/pi/acorn
 rsync -aP ../. /media/$USER/rootfs/home/pi/acorn/
 ls /media/$USER/rootfs/home/pi
--- bringup/bringup-post-flash-raspberrypi.sh
+++ bringup/bringup-post-flash-raspberrypi.sh
@@ -1,3 +1,5 @@
+
+
 sudo apt-get update && sudo apt-get upgrade
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
